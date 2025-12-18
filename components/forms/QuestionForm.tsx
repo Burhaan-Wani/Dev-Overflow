0a80c7bb5242844a17b5f8f17d811979b5286828
Burhaan-U-Deen Wani <burhaanwani823@gmail.com>
2025-12-18 20:17:09 +0530

    implement add tags functionality to ask question form

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,8 +1,18 @@
 "use client";
 
-import { AskQuestionSchema } from "@/lib/validations";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import React, { useRef, useTransition } from "react";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import ROUTES from "@/constants/route";
+import { AskQuestionSchema } from "@/lib/validations";
+
+import TagCard from "../cards/TagCard";
+import { Button } from "../ui/button";
 import {
     Form,
     FormControl,
@@ -13,31 +23,123 @@ import {
     FormMessage,
 } from "../ui/form";
 import { Input } from "../ui/input";
-import dynamic from "next/dynamic";
-import { useRef } from "react";
-import { MDXEditorMethods } from "@mdxeditor/editor";
+import { Loader } from "lucide-react";
 
 const Editor = dynamic(() => import("@/components/Editor"), {
-    // Make sure we turn SSR off
     ssr: false,
 });
 
-const QuestionForm = () => {
+interface Params {
+    question?: Question;
+    isEdit?: boolean;
+}
+
+const QuestionForm = ({ question, isEdit = false }: Params) => {
+    const router = useRouter();
     const editorRef = useRef<MDXEditorMethods>(null);
-    const form = useForm({
+    const [isPending, startTransition] = useTransition();
+
+    const form = useForm<z.infer<typeof AskQuestionSchema>>({
         resolver: zodResolver(AskQuestionSchema),
         defaultValues: {
-            title: "",
-            content: "",
-            tags: [],
+            title: question?.title || "",
+            content: question?.content || "",
+            tags: question?.tags.map((tag) => tag.name) || [],
         },
     });
 
-    const handleCreateQuestion = () => {};
+    const handleInputKeyDown = (
+        e: React.KeyboardEvent<HTMLInputElement>,
+        field: { value: string[] },
+    ) => {
+        console.log(field, e);
+        if (e.key === "Enter") {
+            e.preventDefault();
+            const tagInput = e.currentTarget.value.trim();
+
+            if (
+                tagInput &&
+                tagInput.length < 15 &&
+                !field.value.includes(tagInput)
+            ) {
+                form.setValue("tags", [...field.value, tagInput]);
+                e.currentTarget.value = "";
+                form.clearErrors("tags");
+            } else if (tagInput.length > 15) {
+                form.setError("tags", {
+                    type: "manual",
+                    message: "Tag should be less than 15 characters",
+                });
+            } else if (field.value.includes(tagInput)) {
+                form.setError("tags", {
+                    type: "manual",
+                    message: "Tag already exists",
+                });
+            }
+        }
+    };
+
+    const handleTagRemove = (tag: string, field: { value: string[] }) => {
+        const newTags = field.value.filter((t) => t !== tag);
+
+        form.setValue("tags", newTags);
+
+        if (newTags.length === 0) {
+            form.setError("tags", {
+                type: "manual",
+                message: "Tags are required",
+            });
+        }
+    };
+
+    const handleCreateQuestion = async (
+        data: z.infer<typeof AskQuestionSchema>,
+    ) => {
+        // startTransition(async () => {
+        //     if (isEdit && question) {
+        //         const result = await editQuestion({
+        //             questionId: question?._id,
+        //             ...data,
+        //         });
+        //         if (result.success) {
+        //             toast({
+        //                 title: "Success",
+        //                 description: "Question updated successfully",
+        //             });
+        //             if (result.data)
+        //                 router.push(ROUTES.QUESTION(result.data._id));
+        //         } else {
+        //             toast({
+        //                 title: `Error ${result.status}`,
+        //                 description:
+        //                     result.error?.message || "Something went wrong",
+        //                 variant: "destructive",
+        //             });
+        //         }
+        //         return;
+        //     }
+        //     const result = await createQuestion(data);
+        //     if (result.success) {
+        //         toast({
+        //             title: "Success",
+        //             description: "Question created successfully",
+        //         });
+        //         if (result.data) router.push(ROUTES.QUESTION(result.data._id));
+        //     } else {
+        //         toast({
+        //             title: `Error ${result.status}`,
+        //             description:
+        //                 result.error?.message || "Something went wrong",
+        //             variant: "destructive",
+        //         });
+        //     }
+        // });
+    };
+
     return (
         <Form {...form}>
             <form
-                className="flex w-full flex-col gap-7"
+                className="flex w-full flex-col gap-10"
                 onSubmit={form.handleSubmit(handleCreateQuestion)}
             >
                 <FormField
@@ -100,11 +202,11 @@ const QuestionForm = () => {
                                     <Input
                                         className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
                                         placeholder="Add tags..."
-                                        // onKeyDown={(e) =>
-                                        //     handleInputKeyDown(e, field)
-                                        // }
+                                        onKeyDown={(e) =>
+                                            handleInputKeyDown(e, field)
+                                        }
                                     />
-                                    {/* {field.value.length > 0 && (
+                                    {field.value.length > 0 && (
                                         <div className="flex-start mt-2.5 flex-wrap gap-2.5">
                                             {field?.value?.map(
                                                 (tag: string) => (
@@ -125,7 +227,7 @@ const QuestionForm = () => {
                                                 ),
                                             )}
                                         </div>
-                                    )} */}
+                                    )}
                                 </div>
                             </FormControl>
                             <FormDescription className="body-regular text-light-500 mt-2.5">
@@ -136,21 +238,22 @@ const QuestionForm = () => {
                         </FormItem>
                     )}
                 />
-                <div className="mt-16 flex justify-end">
-                    {/* <Button
+
+                <div className="mt-7 flex justify-end">
+                    <Button
                         type="submit"
                         disabled={isPending}
-                        className="primary-gradient !text-light-900 w-fit"
+                        className="primary-gradient text-light-900! w-fit"
                     >
                         {isPending ? (
                             <>
-                                <ReloadIcon className="mr-2 size-4 animate-spin" />
+                                <Loader className="mr-2 size-4 animate-spin" />
                                 <span>Submitting</span>
                             </>
                         ) : (
                             <>{isEdit ? "Edit" : "Ask a Question"}</>
                         )}
-                    </Button> */}
+                    </Button>
                 </div>
             </form>
         </Form>
