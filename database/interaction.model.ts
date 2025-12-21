ee73e2ce63909ca678bb394b76b8d45f87910003
Burhaan-U-Deen Wani <burhaanwani823@gmail.com>
2025-12-21 18:44:12 +0530

    define various models for app

---
+++ database/interaction.model.ts
@@ -0,0 +1,40 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IInteraction {
+    user: Types.ObjectId;
+    action: string;
+    actionId: Types.ObjectId;
+    actionType: string;
+}
+
+const interactionSchema = new Schema<IInteraction>(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        action: {
+            type: String,
+            required: true,
+        },
+        actionId: {
+            type: Schema.Types.ObjectId, // questionId | answerId
+            required: true,
+        },
+        actionType: {
+            type: String,
+            enum: ["question", "answer"],
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const Interaction =
+    models?.Interaction ||
+    model<IInteraction>("Interaction", interactionSchema);
+
+export default Interaction;
---
+++ database/vote.model.ts
@@ -0,0 +1,37 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IVote {
+    author: Types.ObjectId;
+    actionId: Types.ObjectId;
+    actionType: "question" | "answer";
+    voteType: "upvote" | "downvote";
+}
+
+const voteSchema = new Schema(
+    {
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        actionId: {
+            type: Schema.Types.ObjectId,
+            required: true,
+        },
+        actionType: {
+            type: String,
+            enum: ["question", "answer"],
+            required: true,
+        },
+        voteType: {
+            type: String,
+            enum: ["upvote", "downvote"],
+            required: true,
+        },
+    },
+    { timestamps: true },
+);
+
+const Vote = models?.Vote || model<IVote>("Vote", voteSchema);
+
+export default Vote;
