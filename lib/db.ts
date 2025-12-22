b0d100d37334f8dc7820cca2136f191b36497590
Burhaan-U-Deen Wani <burhaanwani823@gmail.com>
2025-12-22 22:16:19 +0530

    add error handling and logging

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,11 @@
 import mongoose, { Mongoose } from "mongoose";
 
+import logger from "./logger";
+import "@/database";
+
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
-if (MONGODB_URI) {
+if (!MONGODB_URI) {
     throw new Error("MONGODB_URI is not defined");
 }
 
@@ -21,23 +24,29 @@ if (!cached) {
     cached = global.mongoose = { conn: null, promise: null };
 }
 
-const dbConnect = async () => {
+const dbConnect = async (): Promise<Mongoose> => {
     if (cached.conn) {
+        logger.info("Using existing mongoose connection");
         return cached.conn;
     }
+
     if (!cached.promise) {
         cached.promise = mongoose
             .connect(MONGODB_URI, {
-                dbName: "DevFlow",
+                dbName: "devflow",
             })
             .then((result) => {
+                logger.info("Connected to MongoDB");
                 return result;
             })
             .catch((error) => {
+                logger.error("Error connecting to MongoDB", error);
                 throw error;
             });
     }
+
     cached.conn = await cached.promise;
+
     return cached.conn;
 };
 
